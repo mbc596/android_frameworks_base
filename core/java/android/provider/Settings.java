3e3c7fad98f7abfc118e705c13ad3c9a66086b54
drcmda <drcmda@gmail.com>
2013-06-10 09:38:45 +0100

    HALO (1/2)
    HALO is Androids one and only multitasking solution.
    
    We explicitly do not go the Samsung/Cornerstone route. This solution is basically an extension of
    the regular notificationshade, which is an aggregator for notifications that can be checked out when
    time permits. HALO completes it by allowing you to respond to tasks on the spot, trying to be as
    unintrusive as possible and flexible in regards to policies (white/black lists).
    
    Features:
    
    - Apps open up in floating state, apps underneath do not pause
    - complete SystemUI & Core integration, making HALO feel like a native feature, source/SDK and UI/UX
    - HALO has white and blacklists, which for now allow fine control for apps pinging through HALO
    - multiple gestures to navigate HALO:
      - double-tap-move: move
      - tap-drag-left/right: task through notifications, finger up launches
      - tap-drag-up: dismiss notification
      - tap-drag-down: put HALO into semi-hidden state
      - swipe while HALO is hidden wakes it up
      - drag-to-X to remove HALO altogether
    - comes with as few settings as possible, everything should be self explanatory and intutive,
      users must be informed about the double-tap gestures which is the only catch.
    
    Known issues:
    
    - Integration has been finished for PhoneUI. TabletUI and PIE must be integrated still.
    - Packageinstaller must allow HALO through its overlay check or it will disable the install-app button.
    - The low level has a few remaining issues:
      - Some apps still go up fullscale (they trick us with services, circumventing the intent flagging)
      - Some 3D games still pause
      - There is one known app that refuses to end on back or touch-outside, that is WeChat
      - The activitystack pause implementation works but is makeshift
    
    To-Do:
    
    - On-screen tutorial at first start up to teach users the gestures
    - Fixing all known issues, full integration in the remaining UI's
    - Perhaps hybrid engine integration for running floating apps in a decreased size
    
    Merge branch 'multiwindow' into jellybean
    
    Conflicts:
    	core/java/android/app/Activity.java
    	core/java/android/app/INotificationManager.aidl
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    	packages/SystemUI/res/values-land/dimens.xml
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/NotificationData.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/view/PieStatusPanel.java
    	services/java/com/android/server/NotificationManagerService.java
    	services/java/com/android/server/am/ActivityRecord.java
    	services/java/com/android/server/am/ActivityStack.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2159,6 +2159,24 @@ public final class Settings {
          * @hide
          */
         public static final String NOTIFICATION_LIGHT_PULSE = "notification_light_pulse";
+		
+		/**
+         * HALO, should default to 0 (no, do not show)
+         * @hide
+         */
+        public static final String HALO_ACTIVE = "halo_active";
+
+        /**
+         * HALO reversed?, should default to 1 (yes, reverse)
+         * @hide
+         */
+        public static final String HALO_REVERSED = "halo_reversed";
+
+        /**
+         * HALO hide?, should default to 0 (no, do not hide)
+         * @hide
+         */
+        public static final String HALO_HIDE = "halo_hide";
 
         /**
          * Show pointer location on screen?
--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -1536,7 +1536,7 @@ final class ActivityStack {
         
         // We need to start pausing the current activity so the top one
         // can be resumed...
-        if (mResumedActivity != null) {
+        if (mResumedActivity != null && !next.floatingWindow) {
             if (DEBUG_SWITCH) Slog.v(TAG, "Skip resume: need to start pausing");
             // At this point we want to put the upcoming activity's process
             // at the top of the LRU list, since we know we will be needing it
@@ -1615,7 +1615,7 @@ final class ActivityStack {
             if (prev.finishing) {
                 if (DEBUG_TRANSITION) Slog.v(TAG,
                         "Prepare close transition: prev=" + prev);
-                if (mNoAnimActivities.contains(prev)) {
+                if (mNoAnimActivities.contains(prev) || next.floatingWindow) {
                     mService.mWindowManager.prepareAppTransition(
                             AppTransition.TRANSIT_NONE, false);
                 } else {
@@ -1628,7 +1628,7 @@ final class ActivityStack {
             } else {
                 if (DEBUG_TRANSITION) Slog.v(TAG,
                         "Prepare open transition: prev=" + prev);
-                if (mNoAnimActivities.contains(next)) {
+                if (mNoAnimActivities.contains(next) || next.floatingWindow) {
                     noAnim = true;
                     mService.mWindowManager.prepareAppTransition(
                             AppTransition.TRANSIT_NONE, false);
@@ -1760,7 +1760,7 @@ final class ActivityStack {
                 if (!next.hasBeenLaunched) {
                     next.hasBeenLaunched = true;
                 } else {
-                    if (SHOW_APP_STARTING_PREVIEW && mMainStack) {
+                    if (SHOW_APP_STARTING_PREVIEW && mMainStack && !next.floatingWindow) {
                         mService.mWindowManager.setAppStartingWindow(
                                 next.appToken, next.packageName, next.theme,
                                 mService.compatibilityInfoForPackageLocked(
@@ -1794,7 +1794,7 @@ final class ActivityStack {
             if (!next.hasBeenLaunched) {
                 next.hasBeenLaunched = true;
             } else {
-                if (SHOW_APP_STARTING_PREVIEW) {
+                if (SHOW_APP_STARTING_PREVIEW && !next.floatingWindow) {
                     mService.mWindowManager.setAppStartingWindow(
                             next.appToken, next.packageName, next.theme,
                             mService.compatibilityInfoForPackageLocked(
@@ -1893,12 +1893,13 @@ final class ActivityStack {
             }
             if (DEBUG_TRANSITION) Slog.v(TAG,
                     "Prepare open transition: starting " + r);
-            if ((r.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0) {
+
+            if (((r.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0) || (r.floatingWindow && !r.topIntent)) {
                 mService.mWindowManager.prepareAppTransition(
                         AppTransition.TRANSIT_NONE, keepCurTransition);
                 mNoAnimActivities.add(r);
             } else {
-                mService.mWindowManager.prepareAppTransition(newTask
+                mService.mWindowManager.prepareAppTransition(newTask && !r.floatingWindow
                         ? AppTransition.TRANSIT_TASK_OPEN
                         : AppTransition.TRANSIT_ACTIVITY_OPEN, keepCurTransition);
                 mNoAnimActivities.remove(r);
@@ -1920,7 +1921,7 @@ final class ActivityStack {
                     doShow = topRunningNonDelayedActivityLocked(null) == r;
                 }
             }
-            if (SHOW_APP_STARTING_PREVIEW && doShow) {
+            if (SHOW_APP_STARTING_PREVIEW && doShow && !r.floatingWindow) {
                 // Figure out if we are transitioning from another activity that is
                 // "has the same starting icon" as the next one.  This allows the
                 // window manager to keep the previous window it had previously
@@ -3821,7 +3822,7 @@ final class ActivityStack {
                     || (mHistory.get(index-1)).task != r.task;
             if (DEBUG_TRANSITION) Slog.v(TAG,
                     "Prepare close transition: finishing " + r);
-            mService.mWindowManager.prepareAppTransition(endTask
+            mService.mWindowManager.prepareAppTransition(endTask && !r.floatingWindow
                     ? AppTransition.TRANSIT_TASK_CLOSE
                     : AppTransition.TRANSIT_ACTIVITY_CLOSE, false);
     
@@ -4327,7 +4328,7 @@ final class ActivityStack {
         if (top < 0 || (mHistory.get(top)).task.taskId == task) {
             // nothing to do!
             if (reason != null &&
-                    (reason.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0) {
+                    ((reason.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0 || reason.floatingWindow)) {
                 ActivityOptions.abort(options);
             } else {
                 updateTransitLocked(AppTransition.TRANSIT_TASK_TO_FRONT, options);
@@ -4366,7 +4367,7 @@ final class ActivityStack {
         if (DEBUG_TRANSITION) Slog.v(TAG,
                 "Prepare to front transition: task=" + tr);
         if (reason != null &&
-                (reason.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0) {
+                ((reason.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0 || reason.floatingWindow)) {
             mService.mWindowManager.prepareAppTransition(
                     AppTransition.TRANSIT_NONE, false);
             ActivityRecord r = topRunningActivityLocked(null);
@@ -4459,7 +4460,7 @@ final class ActivityStack {
         }
 
         if (reason != null &&
-                (reason.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0) {
+                ((reason.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_ANIMATION) != 0 || reason.floatingWindow)) {
             mService.mWindowManager.prepareAppTransition(
                     AppTransition.TRANSIT_NONE, false);
             ActivityRecord r = topRunningActivityLocked(null);
