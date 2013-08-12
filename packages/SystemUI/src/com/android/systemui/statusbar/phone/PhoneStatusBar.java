242fcfc2ec896faf84d9e6bb676ea37163514104
drcmda <drcmda@gmail.com>
2013-08-12 14:28:21 +0100

    HALO bugfixes
    - couple of problems with concurrent threads
    - green batch only for nap
    - italics gone
    - mdpi and & hdpi icons
    - statusbar-icon gaps
    - flipping batch when content changes
    - batch fadeout without flip
    - code style
    
    Change-Id: Idfb145eb8e220bf224dd3d0e235e3647dcc82ed9
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -579,6 +579,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             // wherever you find it, Quick Settings needs a container to survive
             mSettingsContainer = (QuickSettingsContainerView)
                     mStatusBarWindow.findViewById(R.id.quick_settings_container);
+
             if (mSettingsContainer != null) {
                 mQS = new QuickSettings(mContext, mSettingsContainer);
                 if (!mNotificationPanelIsFullScreenWidth) {
